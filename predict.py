371db8fb5ab92316850ff47a32521bba2655d7ab
namth <tranhainam072@gmail.com>
2023-02-01 20:44:11 +0700

    add onnx model

--- predict.py
+++ predict.py
@@ -1,8 +1,61 @@
 import os
 from argparse import ArgumentParser
+from xml.dom import ValidationErr
 import torch
 from constant import CHECK_POINT
 from transformers import AutoTokenizer, AutoModelForSequenceClassification, TextClassificationPipeline
+import onnx
+import onnxruntime as ort
+from utils import softmax
+
+
+def onnx_pred_fn(path):
+    onnx_model = onnx.load(path)
+    try:
+        onnx.checker.check_model(onnx_model)
+    except onnx.checker.ValidationError as e:
+        print("The model is invalid: %s" % e)
+    else:
+        print("The model is valid!")
+    
+    ort_sess = ort.InferenceSession(path)
+    auto_tokenizer = AutoTokenizer.from_pretrained(CHECK_POINT)
+
+    def pred_fn(input):
+        classes = ['Negative', 'Positive']
+
+        tokenized_input = auto_tokenizer(input, return_tensors="np", truncation=True, padding='max_length', max_length=512)
+
+        outputs = ort_sess.run(None, {
+            'input_ids': tokenized_input['input_ids'],
+            'attention_mask': tokenized_input['attention_mask']
+        })
+
+        result = [{
+            'label': classes[outputs[0][0].argmax(-1)],
+            'score': softmax(outputs[0])[0].max()
+        }]
+        return result
+
+    return pred_fn
+
+
+def base_model_pred_fn(path):
+    model = AutoModelForSequenceClassification.from_pretrained(path)
+    model.config.id2label = {0: 'Negative', 1: 'Positive'}
+
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    device_index = model.device.index if model.device.type != 'cpu' else -1
+    model.to(device)
+
+    auto_tokenizer = AutoTokenizer.from_pretrained(CHECK_POINT)
+    classifier = TextClassificationPipeline(
+        model=model,
+        tokenizer=auto_tokenizer, 
+        device=device_index
+    )
+
+    return classifier
 
 
 if __name__ == "__main__":
@@ -10,26 +63,19 @@ if __name__ == "__main__":
     parser.add_argument("--model-path", default='./model/DistilBert', type=str)
     args = parser.parse_args()
 
-    print('---------------------Welcome to ProtonX DistilBert-------------------')
-    
-    device = 'cuda' if torch.cuda.is_available() else 'cpu'
-    loaded_model = AutoModelForSequenceClassification.from_pretrained(args.model_path)
-    loaded_model.to(device)
+    print('---------------------Welcome to ProtonX -------------------')
 
-    device_index = loaded_model.device.index if loaded_model.device.type != 'cpu' else -1
-    auto_tokenizer = AutoTokenizer.from_pretrained(CHECK_POINT)
+    if os.path.splitext(args.model_path)[1] == '.onnx':
+        classifier = onnx_pred_fn(args.model_path)
+    else:
+        classifier = base_model_pred_fn(args.model_path)
 
-    classifier = TextClassificationPipeline(
-        model=loaded_model,
-        tokenizer=auto_tokenizer, 
-        device=device_index
-    )
-    loaded_model.config.id2label = {0: 'Negative', 1: 'Positive'}
 
     text = input("Input text to analyze sentiment: ")
     while text != 'q':
         sent, prob = classifier(text)[0]['label'], 100*classifier(text)[0]['score']
         print(f"{sent} sentiment: {prob:.2f}%")
+
         text = input("Input text to analyze sentiment: ")
     
 
--- utils.py
+++ utils.py
@@ -1,4 +1,7 @@
 import numpy as np
+import torch
+from transformers import AutoTokenizer
+from constant import CHECK_POINT
 
 def compute_metrics(eval_pred):
     predictions = eval_pred.predictions
@@ -6,3 +9,56 @@ def compute_metrics(eval_pred):
     predictions = np.argmax(predictions, axis=1)
     return {"accuracy": (predictions == labels).mean()}
 
+
+def softmax(tensor):
+    e = np.exp(tensor)
+    return e / e.sum()
+
+
+def export_onnx_file(model, path='model/torch-model.onnx'):
+    # set the model to inference mode 
+    model.eval() 
+
+    sentence = 'This is a perfect series for family viewing'
+    auto_tokenizer = AutoTokenizer.from_pretrained(CHECK_POINT)
+    dummy_model_input = auto_tokenizer(sentence, return_tensors="pt")
+
+    torch.onnx.export(
+        model,                                          # model being run 
+        tuple(dummy_model_input.values()),              # model input
+        f=path,                                         # where to save the model  
+        # export_params=True,                             # store the trained parameter weights inside the model file 
+        input_names=['input_ids', 'attention_mask'],    # the model's input names 
+        output_names=['logits'],                        # the model's output names 
+        dynamic_axes={
+            'input_ids': {0: 'batch_size', 1: 'sequence'},        # variable length axes 
+            'attention_mask': {0: 'batch_size', 1: 'sequence'}, 
+            'logits': {0: 'batch_size', 1: 'sequence'}}, 
+        do_constant_folding=True,                       # whether to execute constant folding for optimization 
+        opset_version=10,                               # the ONNX version to export the model to 
+    )
+
+
+
+def accuracy_onnx_fn(path, dataset, batch_size=16):
+    input_ids = dataset['input_ids'].numpy()
+    attention_mask = dataset['attention_mask'].numpy()
+    labels = dataset['label'].numpy()
+
+    ort_sess = ort.InferenceSession(path)
+    accuracy = 0.0
+    total = len(labels)
+    n = 0
+
+    while n < total:
+        print(n)
+        outputs = ort_sess.run(None, {
+            'input_ids': input_ids[n:(n+batch_size)],
+            'attention_mask': attention_mask[n:(n+batch_size)]
+        })
+
+        accuracy += (outputs[0].argmax(-1) == labels[n:(n+batch_size)]).sum()
+        n += batch_size
+
+    accuracy = (100 * accuracy / total)
+    return accuracy
